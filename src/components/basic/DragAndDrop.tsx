882cae40de1b28df1593cc99d7b27847e93ed858
mattraydev <cedaring.xr@gmail.com>
2023-09-23 21:00:37 -0600

    minor changes

--- src/components/basic/DragAndDrop.tsx
+++ src/components/basic/DragAndDrop.tsx
@@ -1,13 +1,42 @@
 import React, { useState, useRef } from 'react'
 import { BsStack } from 'react-icons/bs'
 
-function DragAndDrop() {
+const itemList = [
+	{
+		id: 1,
+		name: 'Item 1'
+	},
+	{
+		id: 2,
+		name: 'Item 2'
+	},
+	{
+		id: 3,
+		name: 'Item 3'
+	}
+]
+
+const DragAndDrop = () => {
 	const [draggableItems, setDraggableItems] = useState(['Item 1', 'Item 2', 'Item 3'])
+	const [inputValue, setInputValue] = useState<string>('')
 
 	let draggingItem = useRef()
 	let dragOver = useRef()
 
-	function handleDrag(e, index) {
+	const handleAddItem = (e) => {
+		//append new item onto draggable items list
+		console.log('adding list item')
+		e.preventDefault()
+		setInputValue(inputValue)
+		//add inputValue to draggable items list
+		const newItem = {
+			id: draggableItems.length,
+			name: inputValue
+		}
+		// setDraggableItems(draggableItems.push(newItem))
+	}
+
+	const handleDrag = (e, index) => {
 		draggingItem = index
 	}
 
@@ -20,26 +49,38 @@ function DragAndDrop() {
 	return (
 		<section className="component">
 			<h2 className="component-title">Drag re-order and add Stack</h2>
-			<div className="add-drad-item">
-				<input type="text" name="draggable-item-name" placeholder="add item" className="input" />
-				<button className="button">Add Item</button>
+			<div className="">
+				<input
+					type="text"
+					name="draggable-item-name"
+					placeholder="add item"
+					className="input"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+				/>
+				<button className="button" onClick={(e) => handleAddItem(e, inputValue)}>
+					Add Item
+				</button>
 			</div>
-			<div className="drag-container">
-				{draggableItems.map((item, index) => (
-					<div
-						key={index}
-						className="draggable flex flex-row"
-						draggable="true"
-						onDragStart={(e) => handleDrag(e, index)}
-						onDragOver={(e) => handleDragOver(e, index)}
-						onDragEnd={(e) => handleDrop(e, index)}
-					>
-						<div className="hamburger">
-							<BsStack className="text-2xl m-2" />
+			<div className="flex flex-row">
+				<div className="border-[1px] border-content p-1 m-1 w-[45%]">
+					{draggableItems.map((item, index) => (
+						<div
+							key={index}
+							className="draggable flex flex-row border-content border-2 m-2 py-4 rounded-md"
+							draggable
+							onDragStart={(e) => handleDrag(e, index)}
+							onDragOver={(e) => handleDragOver(e, index)}
+							onDragEnd={(e) => handleDrop(e, index)}
+						>
+							<div>
+								<BsStack className="text-2xl m-2" />
+							</div>
+							<p className="mt-2 text-content">{item}</p>
 						</div>
-						<p className="mt-2">{item}</p>
-					</div>
-				))}
+					))}
+				</div>
+				<div className="border-[1px] border-content p-1 m-1 w-[45%]"></div>
 			</div>
 		</section>
 	)
